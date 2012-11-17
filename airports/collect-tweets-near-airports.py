acacca8652a7d187d0ff4197cf0c74c50452c034
Sean Brennan <whalesonstilts2012@gmail.com>
2012-11-17 16:13:14 -0500

    modifies collect-tweets-near-airports.py to persist data to dev.fount.in

--- airports/collect-tweets-near-airports.py
+++ airports/collect-tweets-near-airports.py
@@ -1,4 +1,15 @@
+
+from twython import Twython
+import urllib2
+import time
+import json
+import signal
+import sys
+import httplib
 import traceback
+import couchdb
+import datetime
+import re
 
 class Airport:
     def __init__(self, name, code, lat, lon):
@@ -8,6 +19,52 @@ class Airport:
         self.lon = lon
     def __str__(self):
         return 'Airport %s (%s): [%f, %f]' % (code, name, lat, lon)
+
+def log(s):
+    print '%s: %s' % (datetime.datetime.today(), s)
+
+def openOrCreateDb(server, name):
+    try:
+        db = server.create(name) # newly created
+        return db
+    except couchdb.http.PreconditionFailed:
+        db = server[name]
+        return db
+
+def saveObjectToCouch(db, o):
+    o['_id'] = o['id_str']
+    try:
+        db.save(o, batch='ok')
+        return True
+    except couchdb.http.ResourceConflict:
+        print('Object with _id %s already in db; continuing...' % o['_id'])
+        return False
+    except (socket.error, couchdb.http.ServerError):
+        print 'Caught error on db.save(), sleeping for a while...'
+        time.sleep(60) # hope db comes back up
+        return False
+
+def updateVitals(db):
+    status = {
+        '_id': instance_id,
+        'num_tweets': numTweets,
+        'num_geo_tweets': numGeoTweets,
+        'num_non_geo_tweets': numNonGeoTweets,
+        'started_at': startTime,
+        'last_update': str(datetime.datetime.utcnow()),
+        'pause_time': pauseTime,
+        'db_name': dbName,
+        'geo_or_all': geoOrAll }
+    try:
+        doc = db[status['_id']]
+        status['_rev'] = doc['_rev']
+    except couchdb.http.ResourceNotFound:
+        pass
+    try:
+        db.save(status, batch='ok')
+    except (socket.error, couchdb.http.ServerError):
+        time.sleep(60)
+        pass
 
 def printException():
     exc_type, exc_value, exc_traceback = sys.exc_info()
@@ -49,12 +106,6 @@ input.close()
 
 ## --------------------------------------------------------------------------------
 ## Collect tweets
-from twython import Twython
-import time
-import json
-import signal
-import sys
-import httplib
 
 # Catch ctrl+C
 def signal_handler(signal, frame):
@@ -64,79 +115,113 @@ def signal_handler(signal, frame):
         sys.exit(0)
 signal.signal(signal.SIGINT, signal_handler)
 
-""" Instantiate Twython with no Authentication """
-twitter = Twython()
-
 #Geo_area = "34.05,-118.25,150km" # Los Angeles
 #geo_area = "43.165556,-77.611389,80km" # Rochester, NY
 #geo_area = "40.716667,-74.00,100km" # NYC
 
-
+dbName = 'airport_tweets'
 lastID = []
 numTweets = 0
+numGeoTweets = 0
+numNonGeoTweets = 0
 gps = 0
 location = 0
 numSuccessfulQueries = 0
 waitTime = 5 # minutes
-pauseTime = float(sys.argv[3]) # minutes
+rexLatLon = re.compile(r'(?P<lat>[-]*[0-9]+\.[0-9]+)[^0-9-]+(?P<lon>[-]*[0-9]+\.[0-9]+)')
+pNewLine = re.compile('[\r\n]+')
 
 begin = int(sys.argv[1])
 end = int(sys.argv[2])
+pauseTime = float(sys.argv[3]) # minutes
+geoOrAll = sys.argv[4].strip().lower()
 print 'Collecting airports %d through %d' % (begin, end)
+
+ip_addr = urllib2.urlopen("http://automation.whatismyip.com/n09230945.asp").read()
+instance_id = '%s (%s) @ %s' % (dbName, geoOrAll, ip_addr)
+
+""" Instantiate Twython with no Authentication """
+twitter = Twython()
+
+""" instantiate couchdb connections """
+couch = couchdb.Server('http://dev.fount.in:5984')
+couch.resource.credentials = ('admin', 'admin')
+db_status = openOrCreateDb(couch, 'demon_status') 
+db = openOrCreateDb(couch, dbName)
 
 for a in range(begin,end+1):
     lastID.append(0)
 
+startTime = str(datetime.datetime.utcnow())
+updateVitals(db_status)
+
+# NOTE: radius is tight enough that we are not worrying about intersections.
+# if it expands, it's something to reconsider.
 while True:
     for (i, a) in enumerate(range(begin,end+1)):
         airport = code2a[codes[a]]
-        fileName = 'log/log-geo-area_%s.txt' % airport.code
-        log = open(fileName, 'a', 0)
         radius = '3km'
         geo_area = '%f,%f,%s' % (airport.lat, airport.lon, radius)
+
+        # perform search over geographic area
         try:
             search_results = twitter.searchTwitter(q="", rpp="100", lang="en", geocode=geo_area, since_id=str(lastID[i]), result_type="recent")
-        #except (ValueError, AttributeError, httplib.BadStatusLine):
         except: # catch all problems
             print 'Error caught, continuing after %d seconds' % (waitTime*60)
-            log.close()
             print i, a
             printException()
-            time.sleep(waitTime *60)
+            time.sleep(waitTime)
             twitter = Twython()
             continue
+
         #print search_results
         try:
             e = search_results['error']
             print 'Error caught: %s' % e
             print 'Waiting for %d minutes' % waitTime
-            time.sleep(waitTime * 60)
-            log.close()
+            time.sleep(waitTime)
             continue
         except KeyError:
             numSuccessfulQueries += 1
+
+        # stall if no results key came back
+        if not(search_results.has_key("results")):
+            log('Error caught: no "results" key')
+            log('  Waiting for %d seconds' % waitTime)
+            time.sleep(waitTime)
+            twitter = Twython()
+            continue
 
         for tweet in search_results["results"]:
             numTweets += 1
             if lastID[i] < tweet["id"]: 
                 lastID[i] = tweet["id"]
-            log.write(json.dumps(tweet))
-            log.write('\n')
-            if numTweets % 1000 == 0:
-                #print "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
-                print str(numTweets)+" tweets logged, "+str(location)+" have at least symbolic location info, "+str(gps)+" have specific GPS field filled in."
-                #print "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
-            try:        
-                if  tweet["geo"] != None:
-                    gps += 1
-                elif  tweet["location"] != None:
-                    location += 1
-            except KeyError:
-                pass
 
-            print '%s: %s' % (airport.code, tweet["text"].encode('utf-8'))
+            # Extract GPS: try 'geo' tag, fallback to 'location' tag
+            if not('geo' in tweet) and tweet['location'] != None:
+                 match = rexLatLon.search(tweet['location'])
+                 if bool(match):
+                     (lat, lon) = float(match.group('lat')), float(match.group('lon'))
+                     tweet['geo'] = {'type': 'Point', 'coordinates': (lat, lon)}
+            try:
+                if len(tweet['geo']['coordinates']) != 2:
+                    tweet['geo'] = None
+            except (KeyError, TypeError):
+                tweet['geo'] = None
+
+            # Remove newlines
+            tweet['text'] = re.sub(pNewLine, ' ', tweet['text']).encode("utf-8")
+            # save airport code in the tweet
+            tweet['airport'] = airport.code
+
+            # persist tweets in database
+            if geoOrAll == 'all' or tweet['geo'] != None:
+                saveObjectToCouch(db, tweet)
+
+            print '%s: %s' % (airport.code, tweet["text"])
+            if numTweets % 500 == 0:
+                updateVitals(db_status)
         sys.stdout.flush()
-        log.close()
     time.sleep(pauseTime * 60)
 
 
