26bef57a3d9d045b0b2d9562e71b37d89de9f6df
Sean Brennan <whalesonstilts2012@gmail.com>
2012-11-23 14:28:42 -0500

    refactoring to make it not stupid

--- airports/stream-interesting-people.py
+++ airports/stream-interesting-people.py
@@ -66,98 +66,98 @@ class DbManager:
             print('Database instance for "%s" has not been opened or created.' % self.name)
             return False
 
+""" function for writing status """
+def updateVitals(db, name, numPeople, numInteresting):
+    idFor = '%s (%s) @ %s' % (name + '_interesting', "N/A", ip_addr)
+    doc = db.get(idFor)
+    try:
+        status = {
+            '_id': idFor,
+            'num_tweets': numTweets,
+            'num_geo_tweets': numGeoTweets,
+            'num_non_geo_tweets': numNonGeoTweets,
+            'num_people': numPeople,
+            'num_interesting': numInteresting,
+            'started_at': start_time,
+            'last_update': str(datetime.datetime.utcnow()),
+            'pause_time': pause_time,
+            'db_name': name }
+        if doc != None:
+            status['_rev'] = doc['_rev']
+    except couchdb.http.ResourceNotFound:
+        pass
+    try:
+        db.save(status)
+    except (socket.error, couchdb.http.ServerError) as e:
+        print "Couldn't write status to server: %s" % e
+        sys.exit()
+
+""" utility function for streaming -> search """
+def rest2search(stream):
+    search = {}
+    search['_id'] = stream['id_str']
+    search['id'] = stream['id']
+    search['id_str'] = stream['id_str']
+    search['text'] = stream['text']
+    search['source'] = stream['source']
+    search['created_at'] = stream['created_at']
+    search['geo'] = None
+    if stream['geo'] != None:
+        search['geo'] = stream['geo']
+    search['metadata'] = None
+    search['to_user'] = stream['in_reply_to_screen_name']
+    search['to_user_id'] = stream['in_reply_to_user_id']
+    search['to_user_id_str'] = stream['in_reply_to_user_id_str']
+    search['to_user_name'] = '' # we'd have to look this up in the REST API, not worth it imo
+    search['from_user'] = stream['user']['screen_name']
+    search['from_user_id'] = stream['user']['id']
+    search['from_user_id_str'] = stream['user']['id_str']
+    search['from_user_name'] = stream['user']['name']
+    search['iso_language_code'] = stream['user']['lang']
+    search['profile_image_url'] = stream['user']['profile_image_url']
+    search['profile_image_url_https'] = stream['user']['profile_image_url_https']
+    # stuff our app uses
+    search['origin'] = 'rest'
+    search['airport'] = None
+    return search
+
+""" sleep if rate limit is hit """
+def backoff():
+    current_time = int(datetime.datetime.utcnow().strftime("%s"))
+    current_delta = current_time - last_time
+    refresh_delta = refresh_time - current_delta + 1
+    print "Sleeping for %s seconds..." % refresh_delta
+    time.sleep(refresh_delta)
+
+""" initialize cmdline args and other parameters """
+pause_time = float(sys.argv[1])
+refresh_time = int(sys.argv[2])
+last_time = int(datetime.datetime.utcnow().strftime("%s"))
+dbname = 'airport_tweets'
+numTweets = 0
+numGeoTweets = 0
+numNonGeoTweets = 0
+interestingPeople = []
+ip_addr = urllib2.urlopen("http://automation.whatismyip.com/n09230945.asp").read()
+start_time = str(datetime.datetime.utcnow())
+username = 'corporaMine'
+password = 'tw1tterhea1th'
+rexLatLon = re.compile(r'(?P<lat>[-]*[0-9]+\.[0-9]+)[^0-9-]+(?P<lon>[-]*[0-9]+\.[0-9]+)')
+pNewLine = re.compile('[\r\n]+')
+
+""" initialize Twython """
+twitter = Twython()
+
+""" initialize CouchDB stuff """
+couch = couchdb.Server('http://dev.fount.in:5984')
+couch.resource.credentials = ('admin', 'admin')
+db_status = DbManager('demon_status', couch)
+db_status.open()
+db_airport_tweets = DbManager(dbname, couch)
+db_airport_tweets.open()
+
 """ execute the script """
 if __name__ == "__main__":
-    """ initialize cmdline args and other parameters """
-    pause_time = float(sys.argv[1])
-    refresh_time = int(sys.argv[2])
-    last_time = int(datetime.datetime.utcnow().strftime("%s"))
-    dbname = 'airport_tweets'
-    numTweets = 0
-    numGeoTweets = 0
-    numNonGeoTweets = 0
-    interestingPeople = []
-    ip_addr = urllib2.urlopen("http://automation.whatismyip.com/n09230945.asp").read()
-    start_time = str(datetime.datetime.utcnow())
-    username = 'corporaMine'
-    password = 'tw1tterhea1th'
-    rexLatLon = re.compile(r'(?P<lat>[-]*[0-9]+\.[0-9]+)[^0-9-]+(?P<lon>[-]*[0-9]+\.[0-9]+)')
-    pNewLine = re.compile('[\r\n]+')
-
-    """ initialize Twython """
-    twitter = Twython()
-
-    """ initialize CouchDB stuff """
-    couch = couchdb.Server('http://dev.fount.in:5984')
-    couch.resource.credentials = ('admin', 'admin')
-    db_status = DbManager('demon_status', couch)
-    db_status.open()
-    db_airport_tweets = DbManager(dbname, couch)
-    db_airport_tweets.open()
-
-    """ function for writing status """
-    def updateVitals(db, name, numPeople, numInteresting):
-        idFor = '%s (%s) @ %s' % (name + '_interesting', "N/A", ip_addr)
-        doc = db.get(idFor)
-        try:
-            status = {
-                '_id': idFor,
-                'num_tweets': numTweets,
-                'num_geo_tweets': numGeoTweets,
-                'num_non_geo_tweets': numNonGeoTweets,
-                'num_people': numPeople,
-                'num_interesting': numInteresting,
-                'started_at': start_time,
-                'last_update': str(datetime.datetime.utcnow()),
-                'pause_time': pause_time,
-                'db_name': name }
-            if doc != None:
-                status['_rev'] = doc['_rev']
-        except couchdb.http.ResourceNotFound:
-            pass
-        try:
-            db.save(status)
-        except (socket.error, couchdb.http.ServerError) as e:
-            print "Couldn't write status to server: %s" % e
-            sys.exit
-
-    """ utility function for streaming -> search """
-    def rest2search(stream):
-        search = {}
-        search['_id'] = stream['id_str']
-        search['id'] = stream['id']
-        search['id_str'] = stream['id_str']
-        search['text'] = stream['text']
-        search['source'] = stream['source']
-        search['created_at'] = stream['created_at']
-        search['geo'] = None
-        if stream['geo'] != None:
-            search['geo'] = stream['geo']
-        search['metadata'] = None
-        search['to_user'] = stream['in_reply_to_screen_name']
-        search['to_user_id'] = stream['in_reply_to_user_id']
-        search['to_user_id_str'] = stream['in_reply_to_user_id_str']
-        search['to_user_name'] = '' # we'd have to look this up in the REST API, not worth it imo
-        search['from_user'] = stream['user']['screen_name']
-        search['from_user_id'] = stream['user']['id']
-        search['from_user_id_str'] = stream['user']['id_str']
-        search['from_user_name'] = stream['user']['name']
-        search['iso_language_code'] = stream['user']['lang']
-        search['profile_image_url'] = stream['user']['profile_image_url']
-        search['profile_image_url_https'] = stream['user']['profile_image_url_https']
-        # stuff our app uses
-        search['origin'] = 'rest'
-        search['airport'] = None
-        return search
-
-    """ sleep if rate limit is hit """
-    def backoff(last_time, refresh_time):
-        current_time = int(datetime.datetime.utcnow().strftime("%s"))
-        current_delta = current_time - last_time
-        refresh_delta = refresh_time - current_delta + 1
-        print "Sleeping for %s seconds..." % refresh_delta
-        time.sleep(refresh_delta)
-
     """ main program loop """
     while True:
         # query for users -> airports, find cases with more than two airports
@@ -168,8 +168,10 @@ if __name__ == "__main__":
             numPeople += 1
             if len(value) > 1:
                 interestingPeople.append(key)
+
         print "People: %d" % numPeople
         print "Interesting people: %s" % len(interestingPeople)
+        updateVitals(db_status, dbname, numPeople, len(interestingPeople))
 
         # pull in "interesting" tweets via the REST API, slowly
         try:
@@ -179,7 +181,7 @@ if __name__ == "__main__":
                 # check for rate limit, back off if so
                 try:
                     response = tweets['error']
-                    backoff(last_time, refresh_time)
+                    backoff()
                     # reset the stream completely
                     last_time = int(datetime.datetime.utcnow().strftime("%s"))
                     continue
@@ -216,8 +218,9 @@ if __name__ == "__main__":
                 if current_time - last_time >= 60:
                     updateVitals(db_status, dbname, numPeople, len(interestingPeople))
                 # update last_time if ~3600s have elapsed (rate limit)
-                if current_time - last_time >= 3600:
+                if current_time - last_time >= refresh_time:
                     last_time = int(datetime.datetime.utcnow().strftime("%s"))
+            updateVitals(db_status, dbname, numPeople, len(interestingPeople))
         except KeyError as e:
             print "%s" % e
             continue
