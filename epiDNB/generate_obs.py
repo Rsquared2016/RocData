c058b69002e04ecc416b496780515b100f118f47
adam <sadileka@gmail.com>
2012-12-08 17:13:51 -0500

    clean toy DBN; now adding decision tree CPTs for meetings

--- epiDNB/generate_obs.py
+++ epiDNB/generate_obs.py
@@ -4,22 +4,23 @@ numLines = 100
 cardinality = 3
 numColumns = 3
 
-# Write observations for EM
-fout = open('dbn_train_%s.txt' % numLines, 'w')
-# Write true labels
-foutLabels = open('dbn_train_%s_true_labels.txt' % numLines, 'w')
+# Write observations for EM here
+fout = open('data/dbn_train_%d.txt' % numLines, 'w')
+# Write true labels here
+foutLabels = open('data/dbn_train_%d_true_labels.txt' % numLines, 'w')
 
 # Deterministic
 for lineNum in xrange(0,numLines):
 	if lineNum < numLines/3.0:
 		for col in xrange(0, numColumns):
 			fout.write('%d ' % 0)
-			foutLabels.write('%d\n' % 0)
+			foutLabels.write('%d ' % 0)
 	else:
 		for col in xrange(0, numColumns):
 			fout.write('%d ' % 1)
-			foutLabels.write('%d\n' % 1)
+			foutLabels.write('%d ' % 1)
 	fout.write('\n')
+	foutLabels.write('\n')
 
 # # Random	
 # for lineNum in xrange(0,numLines):
--- epiDNB/run.sh
+++ epiDNB/run.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-#test data points: 3000,9000,15000
-#train data points: 900,1500
+python generate_obs.py
 
-./train_cmd.sh 1500
-./test_cmd.sh 15000
+./train_cmd.sh data/dbn_train_100.txt 
+./test_cmd.sh data/dbn_train_100.txt 
