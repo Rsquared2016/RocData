13f62d2aa814bda5fe83264b9791ccee2ed1d4e1
adam <sadileka@gmail.com>
2012-12-09 18:35:12 -0500

    automated DBN composition in python

--- epiDNB/run.sh
+++ epiDNB/run.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-python generate_obs.py &&
+python generate_obs.py
+
+if [ $? -eq 1 ]; then
+	exit 1
+fi
 
 echo
 echo "Triangulating..."
 rm -rf dbn.str.trifile
-gmtkTriangulate -str dbn.str -inputM dbn.mtr &&
+gmtkTriangulate -strFile dbn.str -inputMasterFile dbn.master &&
 
 echo
 echo "Decision tree processing..."
 rm -f dts/*.dts.index
-DT_FILES=$(ls dts/*.dts)
-for f in $DT_FILES
-do
-	gmtkDTindex -decisionTreeFiles $f && true
-done
+gmtkDTindex -inputMasterFile dbn.master && true
 
 ./train_cmd.sh data/dbn_train_100.txt &&
 ./test_cmd.sh data/dbn_train_100.txt 
