b34ead481b8335d478d79e70c57e23840dcc0c0b
Sean Brennan <whalesonstilts2012@gmail.com>
2012-12-09 20:25:42 -0500

    run on machine with more RAM

--- utilities/create-m-toy.py
+++ utilities/create-m-toy.py
@@ -1,6 +1,36 @@
 
 import couchdb
+import math
 import sys
+
+def calcDistance(lat1, lon1, lat2, lon2):                
+    rad = 0.017453292519943
+    yDistance = (lat2 - lat1) * 60.00721
+    xDistance = (math.cos(lat1 * rad) + math.cos(lat2 * rad)) * (lon2 - lon1) * 30.053965
+    distance = math.sqrt( yDistance*yDistance + xDistance*xDistance )
+    return round(distance * 1852.00088832)
+
+def find_closest_city(doc):
+    if len(doc['geo']['coordinates']) == 2:
+        closestCityID = ""
+        minDist = 999999999;
+        for center in centers:
+            dist = calcDistance(doc['geo']['coordinates'][0], doc['geo']['coordinates'][1], center[0], center[1]);
+            if dist < minDist:
+                minDist = dist
+                closestCityID = center[2]
+        return closestCityID
+    else:
+        return None
+
+centers = [
+    [42.3644,-71.059,"BOS"],
+    [40.716667,-74.00,"NYC"],
+    [33.995,-118.063,"LA"],
+    [51.514,-0.122,"LON"],
+    [47.577,-122.229,"SEA"],
+    [37.566,-122.327,"SF"]
+]
 
 couch = couchdb.Server('http://fount.in:5984')
 couch_dev = couchdb.Server('http://dev.fount.in:5984')
@@ -9,29 +39,22 @@ couch_dev.resource.credentials = ('admin', 'admin')
 db = couch['m']
 db_dev = couch_dev['m_toy']
 numTweets = 0
-results_nyc = db.view('Tweet/by_closest_city_day',
+results = db.view('Tweet/by_day_all_geo',
     reduce = False,
     include_docs = True,
-    startkey = ["NYC", 2012, 11, 17],
-    endkey = ["NYC", 2012, 12, 9])
-results_sf = db.view('Tweet/by_closest_city_day',
-    reduce = False,
-    include_docs = True,
-    startkey = ["SF", 2012, 11, 17],
-    endkey = ["SF", 2012, 12, 9])
-print "Grabbing NYC..."
-for row in results_nyc:
+    startkey = [2012, 10, 17],
+    endkey = [2012, 11, 9])
+print "Grabbing NYC and SF..."
+for row in results:
     doc = row.doc
-    db_dev.save(doc)
-    numTweets += 1
-    if numTweets % 50 == 0:
-        print "%s tweets read." % numTweets
-print "Grabbing SF..."
-for row in results_sf:
-    doc = row.doc
-    db_dev.save(doc)
-    numTweets += 1
-    if numTweets % 50 == 0:
-        print "%s tweets read." % numTweets
+    closest = find_closest_city(doc)
+    if closest == "NYC" or closest == "SF":
+        try:
+            db_dev.save(doc)
+            numTweets += 1
+            if numTweets % 50 == 0:
+                print "%s tweets read." % numTweets
+        except ResourceConflict:
+            continue
 print "%s tweets read. Done." % numTweets
     
