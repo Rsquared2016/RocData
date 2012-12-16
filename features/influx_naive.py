d93795c1b93ecd90a88fbeab6696269726ede1c7
Sean Brennan <whalesonstilts2012@gmail.com>
2012-12-15 21:54:41 -0500

    plotting with backtracking + naive influx working on airport_toy

--- features/influx_naive.py
+++ features/influx_naive.py
@@ -26,7 +26,10 @@ def calculate_flux(date, airport):
             num_sick_whole += float(stats['num_sick'])
             if day == date:
                 num_sick_today += float(stats['num_sick'])
-    return (num_sick_today / num_sick_whole) * interval_length
+    try:
+        return (num_sick_today / num_sick_whole) * interval_length
+    except ZeroDivisionError as e:
+        return 0.0
 
 def update_flight_table(date, dest, src, sick, inc):
     if date not in flight_table:
@@ -45,9 +48,9 @@ def update_flight_table(date, dest, src, sick, inc):
 def update_flux_table(date, airport, flux):
     if date not in flux_table:
         flux_table[date] = {}
-    if dest not in flux_table[date]:
-        flux_table[date][dest] = 0.0
-    flux_table[date][dest] = flux
+    if airport not in flux_table[date]:
+        flux_table[date][airport] = 0.0
+    flux_table[date][airport] = flux
 
 def update_user_sick(user, date):
     if user not in user_sick:
@@ -55,7 +58,7 @@ def update_user_sick(user, date):
     user_sick[user] |= set([date])
 
 def user_sick_at_time(user, date):
-    return (user in user_sick and date in user_sick[user])
+    return ((user in user_sick) and (date in user_sick[user]))
 
 def user_sick_over_time(user, start, finish):
     if start > finish:
@@ -101,7 +104,7 @@ print "Tweets read from \"Tweet/flights_to\": %d" % len(results)
 logging.debug("Got %s rows. (\"Tweet/flights_to\")" % len(results))
 for row in results:
     user, time_str, region, doc = int(row.key[0]), row.key[1], row.value, row.doc
-    date = datetime.strptime(doc.key[1], "%Y-%m-%d %H:%M:%S")
+    date = datetime.strptime(time_str.split(" ")[0], "%Y-%m-%d")
     if date < start_date or date > end_date:
         continue
     # add any first encounters
@@ -110,6 +113,8 @@ for row in results:
         flux_table[date] = {}
     if region not in regions:
         regions |= set([region])
+        for day in flux_table.keys():
+            flux_table[day][region] = 0.0
     if user not in users:
         users |= set([user])
         user_timelines[user] = []
@@ -125,10 +130,15 @@ if (end_date - start_date).days + 1 > len(times):
     print 'Not all requested days found in db.'
     exit(-1)
 
+""" initialize flight and flux tables """
+flux_table = { time: { airport: 0.0 for airport in regions } for time in times }
+flight_table = { time: { dest: { src: { 'num_flying': 0, 'num_sick': 0 } for src in regions - set([dest]) } for dest in regions } for time in times }
+
 """ build flight information """
 logging.debug("\nConstructing flight information...")
 for (user, timeline) in user_timelines.items():
     last_point = timeline[0]
+    logging.debug("\tUser %s at %s on %s" % (user, last_point[0], last_point[1]))
     for point in timeline[1:]:
         region_now, time_now = point
         region_last, time_last = last_point
@@ -147,7 +157,7 @@ for (user, flights) in user_flights.items():
         source, destination, departure, arrival = flight
         # checks whether the user was sick over the entire flight time frame
         if user_sick_over_time(user, departure, arrival):
-            logging.debug("\tUser %s was sick on flight %s -> %s" % (user, source, destination))
+            logging.debug("\tUser %s was SICK on flight %s -> %s" % (user, source, destination))
             update_flight_table(arrival, destination, source, True, 1)
         else:
             logging.debug("\tUser %s was fine on flight %s -> %s" % (user, source, destination))
@@ -168,5 +178,6 @@ with open(file_name, 'wb') as pfile:
 # for debugging!
 with open(file_name + 'p', 'w') as pfile:
     pp = pprint.PrettyPrinter(indent = 4, stream = pfile)
-    pp.pprint(meetings)
+    pp.pprint(flux_table)
+    pp.pprint(flight_table)
 logging.debug("\nDone! Exiting...")
--- plot/plot-interesting.py
+++ plot/plot-interesting.py
@@ -22,12 +22,20 @@ def offset_to_key(origin, day_diff):
     d = origin + timedelta(days = day_diff)
     return [airport, d.year, d.month, d.day]
 
-def parse_created_at(created_at):
+def parse_created_at(created_at, day_precision = True):
     # apparently there are two different created_at formats? wtf?
-    try:
-        return datetime.strptime(created_at, '%a %b %d %H:%M:%S +0000 %Y')
-    except ValueError as e:
-        return datetime.strptime(created_at, '%a, %d %b %Y %H:%M:%S +0000')
+    if day_precision:
+        try:
+            truedate = datetime.strptime(created_at, '%a %b %d %H:%M:%S +0000 %Y')
+            return datetime(truedate.year, truedate.month, truedate.day)
+        except ValueError as e:
+            truedate = datetime.strptime(created_at, '%a, %d %b %Y %H:%M:%S +0000')
+            return datetime(truedate.year, truedate.month, truedate.day)
+    else:
+        try:
+            return datetime.strptime(created_at, '%a %b %d %H:%M:%S +0000 %Y')
+        except ValueError as e:
+            return datetime.strptime(created_at, '%a, %d %b %Y %H:%M:%S +0000')
 
 def min_union(a, b):
     return min(set(a) | set(b))
@@ -102,7 +110,7 @@ with open('flu-trends.txt') as gft_file:
 """ retrieve stats from couch db """
 start_key = [airport, start.year, start.month, start.day]
 end_key = [airport, finish.year, finish.month, finish.day]
-query_later = []
+query_later = set()
 query_for = {}
 query_days = {}
 exclude = set()
@@ -131,10 +139,10 @@ for row in results:
         exclude |= set([doc['_id']])
     # else append the from_user_id_str to list to query later
     else:
-        query_later.append(doc['from_user_id_str'])
+        query_later |= set([doc['from_user_id_str']])
         if not(doc['from_user_id_str'] in query_for):
-            query_for[doc['from_user_id_str']] = []
-        query_for[doc['from_user_id_str']].append(result_day)
+            query_for[doc['from_user_id_str']] = set()
+        query_for[doc['from_user_id_str']] |= set([result_day])
         query_days[doc['from_user_id_str']] = set()
     # count total number of users as well
     # weight sum properly if this result was between collection start and the next sunday
@@ -147,7 +155,7 @@ for row in results:
 """ query across all users who MIGHT be sick """
 # do several large queries instead of many many smaller ones
 # this will give us excess data but will be a helluva lot faster
-query_later.sort()
+query_later = sorted(list(query_later))
 step = 100
 for i in range(0, len(query_later), step):
     start_user, finish_user = query_later[i], query_later[min(i + step, len(query_later) - 1)]
@@ -162,22 +170,24 @@ for i in range(0, len(query_later), step):
                 sick_day = parse_created_at(doc['created_at'])
                 for risk_day in [sick_day + timedelta(days = n) for n in range(-2, 3)]:
                     query_days[doc['from_user_id_str']] |= set([risk_day])
-print "\nQuery days: %s" % query_days
+#print "\nQuery days: %s" % query_days
 # now that querying is complete, check whether users were actually sick on those days
 print "\nMerging backtracking results with naive results..."
 for user in query_later:
+    print "\tChecking user %s..." % user
+    print "\t\tSick Days: %s" % query_days[user]
+    print "\t\tCheck Days: %s" % query_for[user]
     for day in query_days[user]:
         # skip if this risk day was not specifically queried for earlier
-        if day not in query_for[user]:
-            continue
-        print "\tUser %s presented health risk on %s!" % (user, day)
-        index = (day - start).days / 7
-        # weight sum properly if this result was between collection start and the next sunday
-        if day >= collect_start and day < collect_start + timedelta(days = 1):
-            couch_values[airport][index] += 1.0 / 1
-        else:
-            couch_values[airport][index] += 1.0 / 7
-        couch_sum += 1
+        if day in query_for[user]:
+            print "\t\tUser %s presented health risk on %s!" % (user, day)
+            index = (day - start).days / 7
+            # weight sum properly if this result was between collection start and the next sunday
+            if day >= collect_start and day < collect_start + timedelta(days = 1):
+                couch_values[airport][index] += 1.0 / 1
+            else:
+                couch_values[airport][index] += 1.0 / 7
+            couch_sum += 1
 
 # also account for collection start here
 if start >= collect_start:
@@ -200,18 +210,28 @@ axes = figure.add_subplot(111)
 gft_times = [key_to_datetime(key) for key in gft_keys[airport]]
 couch_times = [key_to_datetime(key) for key in couch_keys[airport]]
 total_times = [key_to_datetime(key) for key in total_keys[airport]]
-gft_line = axes.plot_date(gft_times, gft_pct, 'r-', linewidth = 1)
-couch_line = axes.plot_date(couch_times, couch_pct, 'b-', linewidth = 1)
-total_line = axes.plot_date(couch_times, total_pct, 'b--', linewidth = 1)
+gft_line = axes.plot_date(gft_times, gft_pct, 'r-', linewidth = 1, label = 'Google Flu Trends')
+couch_line = axes.plot_date(couch_times, couch_pct, 'b-', linewidth = 1, label = 'TwitterHealth Airports (Sick)')
+total_line = axes.plot_date(couch_times, total_pct, 'b--', linewidth = 1, label = 'TwitterHealth Airports (Total)')
+
+""" generate correlation coefficient for lines """
+# just print it out for now
+correlation = np.corrcoef(gft_values[area], couch_values[area])
+print "Correlation: %f" % correlation[0][1]
 
 """ plot styling """
+figure.suptitle("Sick Individuals in %s + Backtracking" % region_map[airport], fontsize = 20)
 axes.xaxis.set_major_locator(mdates.WeekdayLocator(byweekday = 6))
-axes.xaxis.set_major_formatter(mdates.DateFormatter('%B %d'))
+axes.xaxis.set_major_formatter(mdates.DateFormatter('%m/%d'))
 axes.xaxis.set_minor_locator(mdates.DayLocator())
-axes.set_xlabel('Day')
+figure.autofmt_xdate(rotation = 45)
+axes.set_xlabel("Correlation: %f" % correlation[0][1])
 axes.set_ylabel('% Deviation from Average')
-axes.set_xlim(start, finish)
+axes.set_xlim(start, finish - timedelta(days = 7))
 axes.set_ylim(min(-100.0, min_union(gft_pct, couch_pct)), max(100.0, max_union(gft_pct, couch_pct)))
+handles, labels = axes.get_legend_handles_labels()
+axes.legend(handles, labels, loc = 2, prop = { 'size': 8 })
 axes.grid(True)
 
-plt.savefig("figures/%s_idev.png" % airport,  dpi=200, bbox_inches='tight', pad_inches=0, transparent=False)
+todate = datetime.utcnow().strftime("%Y-%m-%d")
+plt.savefig("figures/%s_idev_%s.png" % (airport, todate),  dpi=200, bbox_inches='tight', pad_inches=0, transparent=False)
