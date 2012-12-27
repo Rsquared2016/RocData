f39232a7bd9fa4e1faeaa3d226684f3705293a13
adam <sadileka@gmail.com>
2012-12-27 17:20:00 -0500

    fully connected DBN runs out of RAM, branching to a static DBN with each frame custom coded

--- features/run.sh
+++ features/run.sh
@@ -5,21 +5,21 @@ END="2012-12-9"
 
 echo
 echo "Getting health data from couch..."
-python health.py airport_toy health_toy.pickle 0.8 $START $END
+#3python health.py airport_toy health_toy.pickle 0.8 $START $END
 if [ $? -ne 0 ]; then
 	exit 1
 fi
 
 echo
 echo "Getting meetings from couch..."
-python meetings.py airport_toy meetings_toy.pickle 0.1 1 $START $END
+#python meetings.py airport_toy meetings_toy.pickle 0.1 1 $START $END
 if [ $? -ne 0 ]; then
 	exit 1
 fi
 
 echo
 echo "Checking meetings consistency..."
-python check_consistency.py meetings_toy.pickle check_consistency.dump > /dev/null
+python check_consistency.py meetings_toy.pickle check_consistency.dump
 if [ $? -ne 0 ]; then
 	exit 1
 fi
