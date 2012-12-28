60a1a935d527168a8ccf00cdc233999c3128286c
adam <sadileka@gmail.com>
2012-12-28 18:26:54 -0500

    Revert "fully connected DBN runs out of RAM, branching to a static DBN with each frame custom coded"
    This reverts commit 2807c5f48bb433d9d3a1b85228f106e34bc6fa83.

--- features/check_consistency.py
+++ features/check_consistency.py
@@ -4,7 +4,7 @@
     check_consistency.py <pickle-file-path> <inconsistencies-dump>
 
     example:
-        python check_consistency.py meetings_toy.pickle check_consistency.dump
+        python check_consistency.py meetings_toy.pickle check_consistency.dump > /dev/null
 """
 
 import cPickle as pickle
@@ -17,20 +17,20 @@ ifilename = sys.argv[2]
 inconsistencies = { 'asymmetric': [], 'duplicate': [] }
 
 for (date, user_meetings) in meetings.items():
-    #print "Checking time slice %s ..." % date.strftime("%Y-%m-%d")
+    print "Checking time slice %s ..." % date.strftime("%Y-%m-%d")
     for (user, encounters) in user_meetings.items():
-        #print "\tChecking user %s ..." % user
+        print "\tChecking user %s ..." % user
         for encounter in encounters:
             if user == encounter:
                 print "\t\t\tERROR: %s -> %s is not possible!" % (user, encounter)
                 inconsistencies['duplicate'].append((encounter, user))
                 continue
-            #print "\t\tChecking %s -> %s ..." % (user, encounter)
+            print "\t\tChecking %s -> %s ..." % (user, encounter)
             if (encounter not in meetings[date]) or (user not in meetings[date][encounter]):
                 print "\t\t\tERROR: %s -> %s is not present!" % (encounter, user)
                 inconsistencies['asymmetric'].append((encounter, user))
-            #else:
-            #    print "\t\t\t%s -> %s found, continuing..." % (encounter, user)
+            else:
+                print "\t\t\t%s -> %s found, continuing..." % (encounter, user)
 
 final_number = len(inconsistencies['duplicate']) + len(inconsistencies['asymmetric'])
 print "Final # of inconsistencies: %d" % final_number
@@ -38,4 +38,3 @@ if final_number > 0:
     with open(ifilename, 'w+') as ifile:
         pp = pprint.PrettyPrinter(indent = 4, stream = ifile)
         pp.pprint(inconsistencies)
-    exit(-1)
